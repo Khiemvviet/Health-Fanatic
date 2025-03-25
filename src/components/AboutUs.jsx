1b4ca5e17250147126f91e0fd8451c0ed9dcc2b8
Khiemvviet <vietkhiem2004@gmail.com>
2025-03-25 15:59:56 -0400

    Test

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -8,10 +8,11 @@ const AboutUs = () => {
       <div className=" bg-n-1 px-10 justify-center ">
         <div className="flex flex-col px-5 lg:text-left lg:flex-row py-5 ">
           <div id="Intro" className="w-full text-center lg:text-left lg:pl-[10%] lg:mr-[10%] lg:mt-5">
-            <h1 className="font-bold mt-20 mb-10 text-5xl text-n-4 leading-loose">Winning Formula</h1>
+            <h1 className="font-bold mt-20 mb-10 text-5xl text-n-4 leading-loose">Health Fanatic</h1>
             <p className="text-n-3 text-xl mb-10 leading-relaxed">
-            Welcome to Winning Formula—your ultimate online destination for athlete-focused fitness and wellness. Designed with athletes in mind, Winning Formula combines comprehensive workout plans, expert exercise guides, and tailored nutrition advice to help you reach peak performance.
-            Whether you're an amateur looking to elevate your routine or a seasoned athlete fine-tuning your regimen, Winning Formula provides the resources and tools you need to achieve your fitness goals and maintain a balanced, healthy lifestyle.
+            Welcome to Health Fanatics—your premier online destination for athlete-centered fitness, nutrition, and wellness. Tailored specifically for athletes, Health Fanatics offers expert workout plans, in-depth exercise guides, and personalized nutrition advice to help you unlock your full potential.
+
+            Whether you're an aspiring athlete seeking to enhance your routine or a seasoned professional fine-tuning your regimen, Health Fanatics equips you with the resources and tools needed to reach your fitness goals and sustain a healthy, balanced lifestyle.
             </p>
           </div>
 
